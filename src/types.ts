922002ec79c58e3789709651bc9bd156c7e21f79
bomanaps <mercy.boma35@yahoo.com>
2025-08-12 12:10:48 +0100

    refactor: use type guard for isChromeRequest

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,36 @@
-export type Request = any;
-export const isChromeRequest = (request: Request) : boolean => (typeof (request as any).getContent === 'function')
+export type Header = { name: string; value: string };
+
+export interface RequestData {
+  url: string;
+  postData?: {
+    text?: string;
+  };
+  headers: Header[];
+}
+
+export interface ResponseContent {
+  text?: string;
+  encoding?: string;
+}
+
+export interface ResponseData {
+  content: ResponseContent;
+}
+
+export interface BaseRequest {
+  request: RequestData;
+  response: ResponseData;
+}
+
+export interface ChromeRequest extends BaseRequest {
+  getContent: (callback: (content: string, encoding: string) => void) => void;
+}
+
+export type Request = ChromeRequest | BaseRequest;
+
+export const isChromeRequest = (request: Request): request is ChromeRequest => {
+  if ('getContent' in request) {
+    return typeof request.getContent === 'function';
+  }
+  return false;
+};
